9056cc435e266b5cddcfae5ad2d6b50c6e9a388b
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-12-14 15:03:02 +0000

    Updated tests to resolve controllers

--- Blaise.Api.Tests.Unit/Configuration/UnityTests.cs
+++ Blaise.Api.Tests.Unit/Configuration/UnityTests.cs
@@ -1,5 +1,5 @@
 ﻿using Blaise.Api.Configuration;
-using Blaise.Api.Core.Services;
+using Blaise.Api.Controllers;
 using NUnit.Framework;
 
 namespace Blaise.Api.Tests.Unit.Configuration
@@ -7,33 +7,33 @@ namespace Blaise.Api.Tests.Unit.Configuration
     public class UnityTests
     {
         [Test]
-        public void Given_I_Call_Resolve_For_CatiService_Then_All_Dependencies_Are_Resolved()
+        public void Given_I_Call_Resolve_For_CatiController_Then_All_Dependencies_Are_Resolved()
         {
             //arrange
             var container = UnityConfig.GetConfiguredContainer();
 
             //act && assert
-            Assert.DoesNotThrow(() => container.Resolve(typeof(CatiService), "CatiService"));
+            Assert.DoesNotThrow(() => container.Resolve(typeof(CatiController), "CatiController"));
         }
 
         [Test]
-        public void Given_I_Call_Resolve_For_InstrumentService_Then_All_Dependencies_Are_Resolved()
+        public void Given_I_Call_Resolve_For_InstrumentController_Then_All_Dependencies_Are_Resolved()
         {
             //arrange
             var container = UnityConfig.GetConfiguredContainer();
 
             //act && assert
-            Assert.DoesNotThrow(() => container.Resolve(typeof(InstrumentService), "InstrumentService"));
+            Assert.DoesNotThrow(() => container.Resolve(typeof(InstrumentController), "InstrumentController"));
         }
 
         [Test]
-        public void Given_I_Call_Resolve_For_ServerParkService_Then_All_Dependencies_Are_Resolved()
+        public void Given_I_Call_Resolve_For_ServerParkController_Then_All_Dependencies_Are_Resolved()
         {
             //arrange
             var container = UnityConfig.GetConfiguredContainer();
 
             //act && assert
-            Assert.DoesNotThrow(() => container.Resolve(typeof(ServerParkService), "ServerParkService"));
+            Assert.DoesNotThrow(() => container.Resolve(typeof(ServerParkController), "ServerParkController"));
         }
     }
 }
