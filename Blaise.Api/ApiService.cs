f7d859820b4ea96eb9c5806cdd566a738c9d345c
Nik Webb <nik.webb@ons.gov.uk>
2020-12-15 11:54:51 +0000

    Added windows service for testing

---
+++ Blaise.Api/ApiService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Blaise.Api.Providers;
+using Microsoft.Owin.Hosting;
+
+namespace Blaise.Api
+{
+    partial class ApiService : ServiceBase
+    {
+        public string BaseAddress = ConfigurationProvider.BaseUrl;
+        private IDisposable _server = null;
+        public ApiService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _server = WebApp.Start<Startup>(url: BaseAddress);
+        }
+
+        protected override void OnStop()
+        {
+            _server?.Dispose();
+            base.OnStop();
+        }
+    }
+}
