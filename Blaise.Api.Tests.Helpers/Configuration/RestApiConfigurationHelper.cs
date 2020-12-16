224ccfc73c9f36dd47dc1518efd0d26d0b1aec67
Nik Webb <nik.webb@ons.gov.uk>
2020-12-16 15:59:29 +0000

    Added variable for instrument configuration

--- Blaise.Api.Tests.Helpers/Configuration/RestApiConfigurationHelper.cs
+++ Blaise.Api.Tests.Helpers/Configuration/RestApiConfigurationHelper.cs
@@ -5,5 +5,9 @@ namespace Blaise.Api.Tests.Helpers.Configuration
     public static class RestApiConfigurationHelper
     {
         public static string BaseUrl => ConfigurationExtensions.GetVariable("RestApiBaseUrl");
+
+        public static string InstrumentsUrl =>
+            ConfigurationExtensions.GetVariable(
+                $"/api/v1/serverparks/{BlaiseConfigurationHelper.ServerParkName}/instruments");
     }
 }
