daf696e7ca146c99d0484e235bdc600dd597801d
jamie kerr <jamiestephenkerr@gmail.com>
2020-12-16 09:21:32 +0000

    Fioxed code warningsa from resharper

--- Blaise.Api.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
+++ Blaise.Api.Tests.Helpers/Configuration/BlaiseConfigurationHelper.cs
@@ -9,19 +9,5 @@ namespace Blaise.Api.Tests.Helpers.Configuration
         public static string InstrumentPath => ConfigurationExtensions.GetEnvironmentVariable("InstrumentPath");
         public static string InstrumentName => ConfigurationExtensions.GetEnvironmentVariable("InstrumentName");
         public static string InstrumentPackage => $"{InstrumentPath}//{InstrumentName}.zip";
-
-        public static ConnectionModel BuildConnectionModel()
-        {
-            return new ConnectionModel
-            {
-                ServerName = ConfigurationExtensions.GetEnvironmentVariable("BlaiseServerHostName"),
-                UserName = ConfigurationExtensions.GetEnvironmentVariable("BlaiseServerUserName"),
-                Password = ConfigurationExtensions.GetEnvironmentVariable("BlaiseServerPassword"),
-                Binding = ConfigurationExtensions.GetEnvironmentVariable("BlaiseServerBinding"),
-                Port = ConfigurationExtensions.GetIntEnvironmentVariable("BlaiseConnectionPort"),
-                RemotePort = ConfigurationExtensions.GetIntEnvironmentVariable("BlaiseRemoteConnectionPort"),
-                ConnectionExpiresInMinutes = ConfigurationExtensions.GetIntEnvironmentVariable("ConnectionExpiresInMinutes")
-            };
-        }
     }
 }
