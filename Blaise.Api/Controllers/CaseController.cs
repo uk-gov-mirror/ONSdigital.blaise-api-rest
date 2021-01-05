a204b69151d9a378435a8d8cc9cbe6200a41a037
jamie kerr <jamiestephenkerr@gmail.com>
2021-01-05 13:36:48 +0000

    Added case controller to retreive number of cases

---
+++ Blaise.Api/Controllers/CaseController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+using Blaise.Api.Core.Interfaces.Services;
+using Blaise.Api.Filters;
+using Blaise.Api.Logging.Services;
+
+namespace Blaise.Api.Controllers
+{
+    [ExceptionFilter]
+    [RoutePrefix("api/v1/api/v1/serverparks/{serverParkName}/instruments/{instrumentName}")]
+    public class CaseController : BaseController
+    {
+        private readonly ICaseService _caseService;
+
+        public CaseController(ICaseService caseService)
+        {
+            _caseService = caseService;
+        }
+
+        [HttpGet]
+        [Route("numberofcases")]
+        [ResponseType(typeof(int))]
+        public IHttpActionResult GetNumberOfCases([FromUri] string serverParkName, [FromUri] string instrumentName)
+        {
+            LoggingService.LogInfo($"Obtaining number of cases for instrument {instrumentName}");
+
+            var numberOfCases = _caseService.GetNumberOfCases(instrumentName, serverParkName);
+
+            LoggingService.LogInfo($"Successfully received '{numberOfCases}' cases for instrument {instrumentName}");
+
+            return Ok(numberOfCases);
+        }
+    }
+}
