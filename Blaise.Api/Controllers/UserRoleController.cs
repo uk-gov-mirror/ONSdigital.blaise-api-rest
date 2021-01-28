005ade48f9e897581152155371368da1860c3c49
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-01-28 13:26:08 +0000

    updated role url

--- Blaise.Api/Controllers/UserRoleController.cs
+++ Blaise.Api/Controllers/UserRoleController.cs
@@ -9,7 +9,7 @@ using Blaise.Api.Logging.Services;
 namespace Blaise.Api.Controllers
 {
     [ExceptionFilter]
-    [RoutePrefix("api/v1/users/roles")]
+    [RoutePrefix("api/v1/userroles")]
     public class UserRoleController : BaseController
     {
         private readonly IUserRoleService _roleService;
