7fa08795b00b6274f526082da8672a73c99145d0
Jamie Kerr <jamiestephenkerr@gmail.com>
2021-02-11 19:02:10 +0000

    Reverted Change

--- Blaise.Api.Tests.Helpers/Files/FileSystemHelper.cs
+++ Blaise.Api.Tests.Helpers/Files/FileSystemHelper.cs
@@ -21,11 +21,30 @@ namespace Blaise.Api.Tests.Helpers.Files
             try
             {
                 Thread.Sleep(10000);
-                Directory.Delete(BlaiseConfigurationHelper.TempTestsPath);
+                DeleteDirectoryAndFilesInPath(BlaiseConfigurationHelper.TempTestsPath);
             }
             catch (Exception e)
             {
                 Console.WriteLine($"Could not cleanup folders, {e.Message}, {e}");
+            }
+        }
+
+        public static void DeleteDirectoryAndFilesInPath(string path)
+        {
+            var dirInfo = new DirectoryInfo(path);
+            foreach (var dir in dirInfo.GetDirectories())
+            {
+                foreach (var file in dir.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                dir.Delete(true);
+            }
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                File.Delete(file);
             }
         }
     }
